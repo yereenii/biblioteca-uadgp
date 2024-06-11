4c305cd77178f6253074034397fc5dd4f9c9a548
Yerení JR Jiménez <32132969@uaz.edu.mx>
2024-06-10 21:38:25 -0600

    Feat: Logos y navbar

--- resources/views/navbar-no-auth/navbar.blade.php
+++ resources/views/navbar-no-auth/navbar.blade.php
@@ -43,8 +43,8 @@
 
       <a href="index.html" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">Mentor</h1>
+        <img src="img/logo.svg" alt=""> 
+        {{-- <h1 class="sitename">Mentor</h1> --}}
       </a>
 
       <nav id="navmenu" class="navmenu">
