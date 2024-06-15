e78323c699dd14f45112234b2ca197e989787af9
Yerení JR Jiménez <32132969@uaz.edu.mx>
2024-06-15 14:24:28 -0600

    Feat: footer
    -Se modifican algunos elementos de footer.

--- resources/views/navbar-no-auth/navbar.blade.php
+++ resources/views/navbar-no-auth/navbar.blade.php
@@ -91,13 +91,13 @@
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
           <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Mentor</span>
+            <span class="sitename">UADGP</span>
           </a>
           <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
+            <p>Av. Zacatecas #30, esquina <br> 
+              Av. Hidalgo Guadalupe Zacatecas</p>
+            <p class="mt-3"><strong>Teléfono:</strong> <span>(492)7-68-24-32</span></p>
+            <p><strong>Email:</strong> <span>uadgp@uaz.edu.mx</span></p>
           </div>
           <div class="social-links d-flex mt-4">
             <a href=""><i class="bi bi-twitter-x"></i></a>
@@ -108,7 +108,7 @@
         </div>
 
         <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
+          <h4>Programas Académicos</h4>
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About us</a></li>
@@ -144,14 +144,14 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Mentor</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">UADGP.</strong> <span>Todos los derechos Reservados</span></p>
+     {{--  <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+      </div> --}}
     </div>
 
   </footer>
