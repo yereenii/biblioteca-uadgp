5c17e15508ec7a3b546712c8e55c7ad484ff009b
alexcvsoft <alexcarrillov19@gmail.com>
2024-07-01 21:12:35 -0600

    Se añadió un select Multiple al formulario de asignaión de palabras clave para los documentos

--- resources/views/palabras-clave-documento/form.blade.php
+++ resources/views/palabras-clave-documento/form.blade.php
@@ -11,7 +11,7 @@
                 <select name="documento_id" class="form-control @error('documento') is-invalid @enderror" id="documento">
                     <option value="">Selecciona un Documento para asignar palabras clave</option>
                     @foreach ($documentos as $documento)
-                        <option value="{{ $documento->id }}" {{ old('documento', $palabrasClaveDocumento->documento ?? '') == $documento->id ? 'selected' : '' }}>
+                        <option value="{{ $documento->id }}" {{ old('documento','') == $documento->id ? 'selected' : '' }}>
                             {{ $documento->titulo }}
                         </option>
                     @endforeach
@@ -20,13 +20,33 @@
             {!! $errors->first('documento', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
-            <label for="palabra_clave_id" class="form-label">{{ __('Palabra Clave Id') }}</label>
-            <input type="text" name="palabra_clave_id" class="form-control @error('palabra_clave_id') is-invalid @enderror" value="{{ old('palabra_clave_id', $palabrasClaveDocumento?->palabra_clave_id) }}" id="palabra_clave_id" placeholder="Palabra Clave Id">
+            <label for="palabra_clave" class="form-label">{{ __('Palabras Clave') }}</label>
+            @if ($palabrasClave->isEmpty())
+                <select name="palabra_clave_id" class="form-control @error('palabra_clave') is-invalid @enderror" id="palabra_clave" disabled>
+                    <option value="">Aún no hay palabras clave registradas</option>
+                </select>                
+            @else
+                <select name="palabra_clave_id" class="form-control @error('palabra_clave') is-invalid @enderror" id="palabra_clave" multiple="multiple">
+                    <optgroup label="Selecciona una o más palabras clave">
+                    @foreach ($palabrasClave as $palabraClave)
+                        <option value="{{ $palabraClave->id }}" {{ old('palabra_clave', '') == $palabraClave->id ? 'selected' : '' }}>
+                            {{ $palabraClave->palabra }}
+                        </option>
+                    @endforeach
+                    </optgroup>
+                </select> 
+            @endif
             {!! $errors->first('palabra_clave_id', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
-
     </div>
     <div class="col-md-12 mt20 mt-2">
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </div>
 </div>
+
+@section('js')
+    <script>
+        $('#documento').select2();
+        $('#palabra_clave').select2();
+    </script>
+@endsection
